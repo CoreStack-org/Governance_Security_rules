a01c778d9a436a542140e03d2ac723099a4dbb0a
Akash  S (Intern) <i-akash.s@corestack.io>
2026-03-05 13:00:28 +0530

    test_v1

---
+++ fail1AzureVNetNetworkWatcherValidator.tf
@@ -0,0 +1,12 @@
+
+resource "azurerm_resource_group" "rg_network" {
+  name     = "rg-network-eastus"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-eastus"
+  location            = azurerm_resource_group.rg_network.location
+  resource_group_name = azurerm_resource_group.rg_network.name
+  address_space       = ["10.1.0.0/16"]
+}
